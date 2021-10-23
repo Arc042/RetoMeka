677117dfdd47db27ba9c671abcdb92b492d5fab1
mikelburgoa <74302520+mikelburgoa@users.noreply.github.com>
2021-10-23 19:04:13 +0200

    Epilepsia arreglada

--- vista/js/jquery.js
+++ vista/js/jquery.js
@@ -1,6 +1,8 @@
 /*Carga dinamicamente el header y el footer*/
-$(function(){
+window.onload=function() {
+  $(function(){
     $("#header").load("header.html"); 
     $("#footer").load("footer.html"); 
   });
+}
 /*Fin carga dinamica del header y footer*/
