8b83e7227ab3eb1e815ac94f28ebb1d6fd4f7750
mikelburgoa <74302520+mikelburgoa@users.noreply.github.com>
2021-10-23 12:22:46 +0200

    Header, footer e index linkados via jQuery

---
+++ vista/js/jquery.js
@@ -0,0 +1,6 @@
+/*Carga dinamicamente el header y el footer*/
+$(function(){
+    $("#header").load("header.html"); 
+    $("#footer").load("footer.html"); 
+  });
+/*Fin carga dinamica del header y footer*/
