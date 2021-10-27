923ef851afc6ee07948387a90dcb24a455b73aba
Arc042 <j_santisteban@fpzornotza.com>
2021-10-27 09:21:05 +0200

    alcachofas

--- vista/js/jsFunctions.js
+++ vista/js/jsFunctions.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded",function (event) {
     
     document.getElementById("submit").addEventListener('click', login);
+    document.getElementById("logout").addEventListener('click', logout);
 
+
+    
+    // LOGIN
+	document.getElementById("login").addEventListener('click', function(){
+		document.getElementById("divUser").style.display="block";
+	});
 })
