9316e9b03bce4b8266fbabc01a427400e393ee1a
Arc042 <j_santisteban@fpzornotza.com>
2021-10-28 12:37:10 +0200

    login paraplegico

--- vista/js/jsFunctions.js
+++ vista/js/jsFunctions.js
@@ -2,8 +2,6 @@ document.addEventListener("DOMContentLoaded",function (event) {
     
     document.getElementById("submit").addEventListener('click', login);
     document.getElementById("logout").addEventListener('click', logout);
-
-
     
     // LOGIN
 	document.getElementById("login").addEventListener('click', function(){
