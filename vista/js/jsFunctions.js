f64abebc8b5ad739fb2b69badcf868a12db91792
Arc042 <j_santisteban@fpzornotza.com>
2021-10-28 12:30:52 +0200

    login 80%

--- vista/js/jsFunctions.js
+++ vista/js/jsFunctions.js
@@ -10,3 +10,33 @@ document.addEventListener("DOMContentLoaded",function (event) {
 		document.getElementById("divUser").style.display="block";
 	});
 })
+
+function sessionVarsView()
+{
+	var url = "controller/cSessionVarsView.php";
+	
+	fetch(url, {
+		  method: 'GET', 
+		  headers:{'Content-Type': 'application/json'}  // input data
+		  })
+	.then(res => res.json()).then(result => {
+	
+		console.log(result);
+		
+		if (result.error =="no error"){
+			
+			document.getElementById("msg").innerHTML="You are "+result.user.name+" and type : "+result.user.type;
+			if (result.user.type == 1)
+			{
+				document.getElementById("berria").style.display="inline-block";	
+				document.getElementById("ezabatu").style.display="inline-block";	
+				document.getElementById("aldatu").style.display="inline-block";		
+			}
+			document.getElementById("login").style.display="none";
+			document.getElementById("logout").style.display="inline-block";
+		} else {
+			document.getElementById("msg").innerHTML=result.error;		
+		}
+	})
+	.catch(error => console.error('Error status:', error));			
+}
