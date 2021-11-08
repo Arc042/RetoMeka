06ea0bd0b26a320974e7d47f1eadfcd831143e3f
mikelburgoa <m_burgoa@fpzornotza.com>
2021-11-08 12:52:48 +0100

    quienesSomos

---
+++ vista/js/quienesSomos.js
@@ -0,0 +1,43 @@
+document.getElementById("trabajador").addEventListener("click",function(){
+    Swal.fire({
+        title: 'Manex Hernandez!',
+        text: 'Manex naiz, 19 urteko mutiko Azpeitiarra. Orain dela bi urte amaitu nituen ekoizlari ikasketak, eta gaur egun dron profesionalen ekoizpenean egiten dut lan. Oso gustuko dut enpresa honetan lan egitea, batez ere, une oro lagunartean nagoelako.',        imageUrl: 'https://static.wixstatic.com/media/1b98cb_e5816cbb803b44fd947e826d4f2afa23~mv2.jpg/v1/fill/w_456,h_643,al_c,q_80,usm_0.66_1.00_0.01/manex_edited.webp',
+        imageWidth: 400,
+        imageHeight: 350,
+        imageAlt: 'Custom image',
+      }) 
+})
+
+document.getElementById("trabajador2").addEventListener("click",function(){
+    Swal.fire({
+        title: 'Manex Hernandez!',
+        text: 'Manex naiz, 19 urteko mutiko Azpeitiarra. Orain dela bi urte amaitu nituen ekoizlari ikasketak, eta gaur egun dron profesionalen ekoizpenean egiten dut lan. Oso gustuko dut enpresa honetan lan egitea, batez ere, une oro lagunartean nagoelako.',        imageUrl: 'https://static.wixstatic.com/media/1b98cb_e5816cbb803b44fd947e826d4f2afa23~mv2.jpg/v1/fill/w_456,h_643,al_c,q_80,usm_0.66_1.00_0.01/manex_edited.webp',
+        imageWidth: 400,
+        imageHeight: 350,
+        imageAlt: 'Custom image',
+      }) 
+})
+
+
+document.getElementById("trabajador3").addEventListener("click",function(){
+    Swal.fire({
+        title: 'Manex Hernandez!',
+        text: 'Manex naiz, 19 urteko mutiko Azpeitiarra. Orain dela bi urte amaitu nituen ekoizlari ikasketak, eta gaur egun dron profesionalen ekoizpenean egiten dut lan. Oso gustuko dut enpresa honetan lan egitea, batez ere, une oro lagunartean nagoelako.',        imageUrl: 'https://static.wixstatic.com/media/1b98cb_e5816cbb803b44fd947e826d4f2afa23~mv2.jpg/v1/fill/w_456,h_643,al_c,q_80,usm_0.66_1.00_0.01/manex_edited.webp',
+        imageWidth: 400,
+        imageHeight: 350,
+        imageAlt: 'Custom image',
+      }) 
+})
+
+
+document.getElementById("trabajador4").addEventListener("click",function(){
+    Swal.fire({
+        title: 'Manex Hernandez!',
+        text: 'Manex naiz, 19 urteko mutiko Azpeitiarra. Orain dela bi urte amaitu nituen ekoizlari ikasketak, eta gaur egun dron profesionalen ekoizpenean egiten dut lan. Oso gustuko dut enpresa honetan lan egitea, batez ere, une oro lagunartean nagoelako.',        imageUrl: 'https://static.wixstatic.com/media/1b98cb_e5816cbb803b44fd947e826d4f2afa23~mv2.jpg/v1/fill/w_456,h_643,al_c,q_80,usm_0.66_1.00_0.01/manex_edited.webp',
+        imageWidth: 400,
+        imageHeight: 350,
+        imageAlt: 'Custom image',
+      }) 
+})
+
+
