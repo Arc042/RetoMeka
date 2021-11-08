dad945fa1c5d49e540db11cdaf2707ba28a25b58
mikelburgoa <m_burgoa@fpzornotza.com>
2021-11-08 13:40:40 +0100

    jsPruebas

--- vista/js/productos.js
+++ vista/js/productos.js
@@ -22,6 +22,58 @@ function mostrar() {
     .catch(error => console.log('Error status:', error));
 }
 
-function ordenar() {
+function ordenarAlfabetoAZ() {
 
+    var url = "controlador/controlador_tienda.php";
+	
+	fetch(url, {
+		  method: 'GET', 
+		  headers:{'Content-Type': 'application/json'}  // input data
+		  })
+    .then(res => res.json()).then(result =>{
+
+        console.log('succes:',res.list);
+    
+        var producto = result.list;
+        
+        document.getElementById("user_sort").addEventListener("change", function(){
+
+            for(let i = 0; i<producto.length; i++) {
+    
+                producto += "";
+    
+            }
+            document.getElementById("").innerHTML=producto.short();
+
+        })
+    })
+    .catch(error => console.log('Error status:', error));
+}
+
+function ordenarAlfabetoZA() {
+
+    var url = "controlador/controlador_tienda.php";
+	
+	fetch(url, {
+		  method: 'GET', 
+		  headers:{'Content-Type': 'application/json'}  // input data
+		  })
+    .then(res => res.json()).then(result =>{
+
+        console.log('succes:',res.list);
+    
+        var producto = result.list;
+        
+        document.getElementById("user_sort").addEventListener("change", function(){
+
+            for(let i = 0; i<producto.length; i++) {
+    
+                producto += "";
+    
+            }
+            document.getElementById("").innerHTML=producto.reverse();
+
+        })
+    })
+    .catch(error => console.log('Error status:', error));
 }
