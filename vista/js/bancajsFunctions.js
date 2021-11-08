01fe3c441cc1074b4806b29f707a1796a402b795
Arc042 <j_santisteban@fpzornotza.com>
2021-11-08 14:29:07 +0100

    estilo de pagina bancaria "terminado"

---
+++ vista/js/bancajsFunctions.js
@@ -0,0 +1,19 @@
+function movimientos() {
+    console.log("movimientos")
+}
+
+function prestamo() {
+    console.log("prestamos")
+}
+
+function leasing() {
+    console.log("leasing")
+}
+
+function transferir() {
+    console.log("transferir")
+}
+
+function insertar() {
+    console.log("insertar")
+}
