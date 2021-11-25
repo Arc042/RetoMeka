0f2eaa92c21bc5c32e9cd5ff0e55691434ea65f7
mikelburgoa <m_burgoa@fpzornotza.com>
2021-11-25 12:54:44 +0100

    Quienes somos con nombres

--- vista/js/quienesSomos.js
+++ vista/js/quienesSomos.js
@@ -1,7 +1,7 @@
 document.getElementById("trabajador").addEventListener("click",function(){
     Swal.fire({
-        title: 'Manex Hernandez!',
-        text: 'Manex naiz, 19 urteko mutiko Azpeitiarra. Orain dela bi urte amaitu nituen ekoizlari ikasketak, eta gaur egun dron profesionalen ekoizpenean egiten dut lan. Oso gustuko dut enpresa honetan lan egitea, batez ere, une oro lagunartean nagoelako.',        imageUrl: 'https://static.wixstatic.com/media/1b98cb_e5816cbb803b44fd947e826d4f2afa23~mv2.jpg/v1/fill/w_456,h_643,al_c,q_80,usm_0.66_1.00_0.01/manex_edited.webp',
+        title: 'Saioa Garitagoitia!',
+        text: 'Manex naiz, 19 urteko mutiko Azpeitiarra. Orain dela bi urte amaitu nituen ekoizlari ikasketak, eta gaur egun dron profesionalen ekoizpenean egiten dut lan. Oso gustuko dut enpresa honetan lan egitea, batez ere, une oro lagunartean nagoelako.',        imageUrl: 'https://static.wixstatic.com/media/1b98cb_868a0af3b4f64b49ab0423e589500f7b~mv2.jpg/v1/crop/x_0,y_101,w_1301,h_1301/fill/w_314,h_314,al_c,q_80,usm_0.66_1.00_0.01/saioa_edited.webp',
         imageWidth: 400,
         imageHeight: 350,
         imageAlt: 'Custom image',
@@ -10,8 +10,8 @@ document.getElementById("trabajador").addEventListener("click",function(){
 
 document.getElementById("trabajador2").addEventListener("click",function(){
     Swal.fire({
-        title: 'Manex Hernandez!',
-        text: 'Manex naiz, 19 urteko mutiko Azpeitiarra. Orain dela bi urte amaitu nituen ekoizlari ikasketak, eta gaur egun dron profesionalen ekoizpenean egiten dut lan. Oso gustuko dut enpresa honetan lan egitea, batez ere, une oro lagunartean nagoelako.',        imageUrl: 'https://static.wixstatic.com/media/1b98cb_e5816cbb803b44fd947e826d4f2afa23~mv2.jpg/v1/fill/w_456,h_643,al_c,q_80,usm_0.66_1.00_0.01/manex_edited.webp',
+        title: 'Ianire Romero!',
+        text: 'Manex naiz, 19 urteko mutiko Azpeitiarra. Orain dela bi urte amaitu nituen ekoizlari ikasketak, eta gaur egun dron profesionalen ekoizpenean egiten dut lan. Oso gustuko dut enpresa honetan lan egitea, batez ere, une oro lagunartean nagoelako.',        imageUrl: 'https://static.wixstatic.com/media/1b98cb_ecbb63430cdd453388e888928c842f49~mv2.jpg/v1/crop/x_0,y_46,w_799,h_799/fill/w_314,h_314,al_c,q_80,usm_0.66_1.00_0.01/IANIRE%20ARGAZKI_edited_edited.webp',
         imageWidth: 400,
         imageHeight: 350,
         imageAlt: 'Custom image',
@@ -21,8 +21,8 @@ document.getElementById("trabajador2").addEventListener("click",function(){
 
 document.getElementById("trabajador3").addEventListener("click",function(){
     Swal.fire({
-        title: 'Manex Hernandez!',
-        text: 'Manex naiz, 19 urteko mutiko Azpeitiarra. Orain dela bi urte amaitu nituen ekoizlari ikasketak, eta gaur egun dron profesionalen ekoizpenean egiten dut lan. Oso gustuko dut enpresa honetan lan egitea, batez ere, une oro lagunartean nagoelako.',        imageUrl: 'https://static.wixstatic.com/media/1b98cb_e5816cbb803b44fd947e826d4f2afa23~mv2.jpg/v1/fill/w_456,h_643,al_c,q_80,usm_0.66_1.00_0.01/manex_edited.webp',
+        title: 'Olaia Rivas!',
+        text: 'Manex naiz, 19 urteko mutiko Azpeitiarra. Orain dela bi urte amaitu nituen ekoizlari ikasketak, eta gaur egun dron profesionalen ekoizpenean egiten dut lan. Oso gustuko dut enpresa honetan lan egitea, batez ere, une oro lagunartean nagoelako.',        imageUrl: 'https://static.wixstatic.com/media/1b98cb_8415c8ce2d184bc7b2386b58727391af~mv2.jpg/v1/crop/x_0,y_23,w_599,h_599/fill/w_314,h_314,al_c,q_80,usm_0.66_1.00_0.01/olaia%20argazki_edited.webp',
         imageWidth: 400,
         imageHeight: 350,
         imageAlt: 'Custom image',
